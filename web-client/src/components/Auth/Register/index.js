01504d6452ea1fe27b6e1362d442f5576f230697
nguyenconghieu531 <168156631+NCHieu1210@users.noreply.github.com>
2024-11-07 03:04:37 +0700

    Login and Register

---
+++ web-client/src/components/Auth/Register/index.js
@@ -0,0 +1,109 @@
+import { Button, Form, Input, Modal } from "antd";
+import { useState } from "react";
+import "../Auth.scss";
+import Login from "../Login";
+
+const Register = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <>
+      <Button onClick={() => setModalOpen(true)}>Đăng ký</Button>
+      <Modal
+        title=""
+        centered
+        open={modalOpen}
+        onCancel={() => setModalOpen(false)}
+        footer={null}
+        className="loginModal"
+      >
+        <div className="loginModal__header">
+          <p>log</p>
+          <h2>Đăng ký tài khoản F11</h2>
+          <p>Mỗi người nên sử dụng riêng một tài khoản, tài khoản nhiều người
+            sử dụng chung sẽ bị khóa.</p>
+        </div>
+        <br></br>
+        <Form
+          layout="vertical"
+          name="formLogin"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          size="large"
+          width="100%"
+        >
+          <Form.Item
+            label={<strong>Tên của bạn?</strong>}
+            name="fullname"
+            rules={[
+              {
+                required: true,
+                message: 'Trường này không được để trống!',
+              },
+            ]}
+          >
+            <Input
+              style={{ borderRadius: "50px" }}
+              placeholder="Họ và tên của bạn"
+            />
+          </Form.Item>
+          <Form.Item
+            label={<strong>Email của bạn?</strong>}
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Trường này không được để trống!',
+              },
+            ]}
+          >
+            <Input
+              style={{ borderRadius: "50px" }}
+              placeholder="Email hoặc Username"
+            />
+          </Form.Item>
+
+          <Form.Item
+            label=""
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Trường này không được để trống!',
+              },
+            ]}
+          >
+            <Input.Password
+              style={{ borderRadius: "50px" }}
+              placeholder="Mật Khẩu" />
+          </Form.Item>
+          <br></br>
+          <Form.Item>
+            <Button htmlType="submit">
+              Đăng ký
+            </Button>
+          </Form.Item>
+        </Form>
+        <div className="loginModal__footer">
+          <div>Bạn chưa có tài khoản? <div onClick={() => setModalOpen(false)}><Login /></div></div>
+          <p className="loginModal__footer--question">Quên mật khẩu?</p>
+          <em>Việc bạn tiếp tục sử dụng trang web này đồng nghĩa bạn đồng ý với điều khoản sử dụng của chúng tôi</em>
+        </div>
+
+      </Modal>
+    </>
+  )
+};
+
+export default Register;
--- web-client/src/layout/DefaultLayout/index.js
+++ web-client/src/layout/DefaultLayout/index.js
@@ -1,11 +1,12 @@
 import { NavLink, Outlet } from "react-router-dom";
-import HeaderDefault from "./HeaderDefault";
-import { Button, Layout, Menu, Space } from "antd";
+import { Button, Layout, Space } from "antd";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import "./DefaultLayout.scss";
 import facebook from '../../assets/Imager/logoFacebook.jpg'
 import youtube from '../../assets/Imager/logoYoutube.jpg'
 import tiktok from '../../assets/Imager/logoTiktok.jpg'
+import Login from "../../components/Auth/Login";
+import Register from "../../components/Auth/Register";
 
 const DefaultLayout = () => {
   return (
@@ -18,8 +19,8 @@ const DefaultLayout = () => {
             <NavLink to="#">About</NavLink>
             <NavLink to="#">Service</NavLink>
             <NavLink to="#">Contact US</NavLink>
-            <NavLink className="layoutDefault__header--control--login" to="#"><Button>Login</Button></NavLink>
-            <NavLink className="layoutDefault__header--control--signUp" to="#"><Button>Sign Up</Button></NavLink>
+            <div className="layoutDefault__header--control--login"><Login /></div>
+            <div className="layoutDefault__header--control--signUp"><Register /></div>
           </div>
         </Header>
         <Content>
@@ -27,40 +28,44 @@ const DefaultLayout = () => {
         </Content>
         <Footer className="layoutDefault__footer">
           <table>
-            <tr>
-              <th>Học Lập Trình Để Đi Làm</th>
-              <th>Về Chúng tôi</th>
-              <th>CÔNG TY CỔ PHẦN CÔNG NGHỆ GIÁO DỤC</th>
-              <th>Kết nối với chúng tôi</th>
-            </tr>
-            <tr>
-              <td>Email: <a href="mailto:contact@fullstack.edu.vn">contact@fullstack.edu.vn</a></td>
-              <td><NavLink to={"/#"}>Giới thiệu</NavLink></td>
-              <td>Mã số thuế: 0109922901</td>
-              <td rowSpan="4">
-                <Space>
-                  <a target="_blank" href="https://www.facebook.com"><img src={facebook} alt="facebook" /></a>
-                  <a target="_blank" href="https://www.tiktok.com"><img src={tiktok} alt="tiktok" /></a>
-                  <a target="_blank" href="https://www.youtube.com"><img src={youtube} alt="youtube" /></a>
-                </Space>
-              </td>
-            </tr>
-            <tr>
-              <td>Địa chỉ: Số 1, ngõ 41, Trần Duy Hưng, Cầu Giấy, Hà Nội</td>
-              <td><NavLink to={"/#"}>Liên hệ</NavLink></td>
-              <td>Ngày thành lập: 04/03/2022</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Điện thoại: <a href="tel:0819198989">08 1919 8989</a></td>
-              <td><NavLink to={"/#"}>Điều khoản</NavLink></td>
-              <td>Mã số thuế: 0109922901</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td><NavLink to={"/#"}>Bảo mật</NavLink></td>
-              <td>Lĩnh vực hoạt động: Giáo dục, công nghệ - lập trình. Chúng tôi tập trung xây dựng và phát triển các sản phẩm mang lại giá trị cho cộng đồng lập trình viên Việt Nam.</td>
-            </tr>
+            <thead>
+              <tr>
+                <th>Học Lập Trình Để Đi Làm</th>
+                <th>Về Chúng tôi</th>
+                <th>CÔNG TY CỔ PHẦN CÔNG NGHỆ GIÁO DỤC</th>
+                <th>Kết nối với chúng tôi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Email: <a href="mailto:contact@fullstack.edu.vn">contact@fullstack.edu.vn</a></td>
+                <td><NavLink to={"/#"}>Giới thiệu</NavLink></td>
+                <td>Mã số thuế: 0109922901</td>
+                <td rowSpan="2">
+                  <Space>
+                    <a target="_blank" href="https://www.facebook.com"><img src={facebook} alt="facebook" /></a>
+                    <a target="_blank" href="https://www.tiktok.com"><img src={tiktok} alt="tiktok" /></a>
+                    <a target="_blank" href="https://www.youtube.com"><img src={youtube} alt="youtube" /></a>
+                  </Space>
+                </td>
+              </tr>
+              <tr>
+                <td>Địa chỉ: Số 1, ngõ 41, Trần Duy Hưng, Cầu Giấy, Hà Nội</td>
+                <td><NavLink to={"/#"}>Liên hệ</NavLink></td>
+                <td>Ngày thành lập: 04/03/2022</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Điện thoại: <a href="tel:0819198989">08 1919 8989</a></td>
+                <td><NavLink to={"/#"}>Điều khoản</NavLink></td>
+                <td>Mã số thuế: 0109922901</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td><NavLink to={"/#"}>Bảo mật</NavLink></td>
+                <td>Lĩnh vực hoạt động: Giáo dục, công nghệ - lập trình. Chúng tôi tập trung xây dựng và phát triển các sản phẩm mang lại giá trị cho cộng đồng lập trình viên Việt Nam.</td>
+              </tr>
+            </tbody>
           </table>
           <br></br>
           <br></br>
